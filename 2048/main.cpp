a31aec042530046e30429058cbcda7c1b3a5021e
weizhiyong <weizhiyong@swjtu-richsun.com>
2019-05-30 10:48:15 +0800

    创建程序代码、

---
+++ 2048/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow mainWidget;
+    mainWidget.show();
+
+    return a.exec();
+}
